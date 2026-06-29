46a537c892d93072710d391a079bb4a28d581edc
Mohammad Ayaan Noor <rjayaan722@gmail.com>
2026-06-29 16:10:15 +0530

    ..

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,6 +1,7 @@
 import { createClient } from '@/prismicio'
-import { PrismicNextImage } from '@prismicio/next'
+import { PrismicNextImage, PrismicNextLink } from '@prismicio/next'
 import { Logo } from '@/app/components/Logo'
+import { Bounded } from './bounded'
 
 type Props = {}
 
@@ -13,11 +14,21 @@ export async function Footer({}: Props) {
         <div className='relative h-[75vh] fl-p-10/16 md:aspect-auto'>
         {/* image */}
         <PrismicNextImage field={settings.data.footer_image} alt='' fill className='object-cover'/>
-        </div>
         {/* physics */}
         {/* logo */}
         <Logo className='pointer-events-none relative h-20 mix-blend-exclusion md:h-28'/>
         {/* links */}
+        </div>
+        <Bounded as='nav'>
+            <ul className='flex flex-wrap justify-center gap-8 fl-text-lg/xl'>
+                {settings.data.navigation.map((item)=>(
+                    <li key={item.link.text}>
+                        <PrismicNextLink field={item.link}/>
+
+                    </li>
+                ))}
+            </ul>
+        </Bounded>
     </footer>
   )
 }
